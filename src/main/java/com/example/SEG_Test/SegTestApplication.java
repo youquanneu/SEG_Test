f7af678df8507f87480a4a746a4f1887d507ee78
youquanneu <youquanneu3@gmail.com>
2025-03-08 18:23:41 +0800

    Add files via upload

---
+++ src/main/java/com/example/SEG_Test/SegTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SEG_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SEG_Test/SegTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SEG_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
